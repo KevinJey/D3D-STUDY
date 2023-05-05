bb69b62c0bed430c702028fe2bce3be4af09f520
kevinjey <1617653943@qq.com>
2023-05-05 17:22:26 +0800

    Update Content

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -7,8 +7,19 @@ struct VertexPosColor{
 };
 
 typedef struct D3D11_INPUT_ELEMENT_DESC{
-    LPCSTR SemanticNmae;
-    UINT SemanticIndex;
-    DXGI_FORMAT
-} D3D11_INPUT_ELEMENT_DESC;
+    LPCSTR SemanticName;        // 语义名
+    UINT SemanticIndex;         // 语义索引
+    DXGI_FORMAT Format;         // 数据格式
+    UINT InputSlot;             // 输入槽索引(0-15)
+    UINT AlignedByteOffset;     // 初始位置(字节偏移量)      
+    D3D11_INPUT_CLASSIFICATION InputSlotClass; // 输入类型
+    UINT InstanceDataStepRate;  // 忽略
+}     D3D11_INPUT_ELEMENT_DESC;
+
+// input layout初始化
+const D3D11_INPUT_ELEMENT_DESC VertexPosColor::inputLayout[2] = {
+    { "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+    { "COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0}
+};
+
 
